afc9ae01b93d2f5679b1e6e883e568652dcc94dd
石帅 <804826526@qq.com>
2019-12-21 15:49:52 +0800

    练习了：查看多继承对象的虚表

---
+++ test_2019_12_21_多态/test_2019_12_21_多态/test.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Base1 {
+public:
+	virtual void func1() { cout << "Base1::func1" << endl; }
+	virtual void func2() { cout << "Base1::func2" << endl; }
+private:
+	int b1;
+};
+class Base2 {
+public:
+	virtual void func1() { cout << "Base2::func1" << endl; }
+	virtual void func2() { cout << "Base2::func2" << endl; }
+private:
+	int b2;
+};
+
+class Derive : public Base1, public Base2 {
+public:
+	virtual void func1() { cout << "Derive::func1" << endl; }
+	virtual void func3() { cout << "Derive::func3" << endl; }
+private:
+	int d1;
+};
+
+typedef void(*vfptr)();
+
+void PrintVfptr(vfptr* p)
+{
+	while (*p)
+	{
+		vfptr tmp = *p;
+		cout << "µØÖ·£º" << tmp << "  ";
+		tmp();
+		++p;
+	}
+}
+
+int main()
+{
+	Base1 b1;
+	Base2 b2;
+	Derive d1;
+
+	
+
+	cout << "Base1µÄ£º" << endl;
+	vfptr* p1 = (vfptr*)(*((int*)(&d1)));
+	PrintVfptr(p1);
+
+	cout << "Base2µÄ£º" << endl;
+	vfptr* p2 = (vfptr*)*((int*)((Base1*)(&d1) + 1));
+
+	PrintVfptr(p2);
+
+	system("pause");
+	return 0;
+}
