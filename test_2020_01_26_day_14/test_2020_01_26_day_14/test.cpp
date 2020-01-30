3cb65aa3d16b10717a4730ba3b9ae9541fff6263
石帅 <804826526@qq.com>
2020-01-30 15:23:02 +0800

    练习了：通配符匹配

--- test_2020_01_26_day_14/test_2020_01_26_day_14/test.cpp
+++ test_2020_01_26_day_14/test_2020_01_26_day_14/test.cpp
@@ -70,3 +70,52 @@ public:
 		}
 	}
 };
+
+
+class Solution {
+public:
+	bool isMatch(string s, string p) {
+		int s_size = s.size();
+		int p_size = p.size();
+		int i = 0;
+		int j = 0;
+		int star_i = 0;
+		int star_j = 0;
+		int flag = 0;
+		while (i < s_size)
+		{
+			if (j < p_size && (s[i] == p[j] || p[j] == '?'))//如果是普通的匹配，则++
+			{
+				++i;
+				++j;
+			}
+			else if (j < p_size && p[j] == '*')//如果遇到了*，则用stari记录当前i的位置，starj记录*后面的位置，准备开始用*后面的字母来匹配s里的字母
+			{
+				flag = 1;
+				star_i = i;
+				star_j = ++j;
+			}
+			else if (1 == flag)//如果有进入上面的if，flag会置为1，则说明前面正在进行*的匹配字母
+			{//如果以star_i位置的后面的字符串和star_j后面的字符串不完全匹配，则将之前star_i（也就是上一次开始匹配star_j的首字母）++，也就是再从star_i+1为首的字符串再开始匹配
+				i = ++star_i;
+				j = star_j;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		while (j < p_size && p[j] == '*')//上面的while退出是i走到头了，但是j并不一定，这里需要将j往后走，如果遇到*则表示，*是匹配的空字符
+		{
+			++j;
+		}
+		if (j == p_size)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
