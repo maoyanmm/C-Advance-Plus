ece39e2dad2b70f14cdf6408a78992f58cef0327
石帅 <804826526@qq.com>
2020-02-07 14:43:11 +0800

    练习了：公交车路线

--- test_2020_01_26_day_14/test_2020_01_26_day_14/test.cpp
+++ test_2020_01_26_day_14/test_2020_01_26_day_14/test.cpp
@@ -119,3 +119,52 @@ public:
 		}
 	}
 };
+
+class Solution {
+public:
+	int numBusesToDestination(vector<vector<int>>& routes, int S, int T) {
+		if (S == T)
+		{
+			return 0;
+		}
+		queue<int> myque;//用来形式广度优先搜索
+		myque.push(S);//将出发站push
+		int mini_steps = 1;//最少要坐一站路
+		vector<bool> have_gone(routes.size(), false);//用来判断该公交车是否坐过
+		unordered_map<int, vector<int>> umap;//该map存储两个元素：1、某个站点 2、能到达该站点的公交车（路线）数组
+		for (int i = 0; i < routes.size(); ++i)//遍历所有的公交车（路线）
+		{
+			for (auto& e : routes[i])//遍历某个公交车（路线）可以到达的所有站点
+			{
+				umap[e].push_back(i);//将站点作为key值，将公交车（可以到达该站点的公交车集合）数组作为value
+			}
+		}
+		while (!myque.empty())
+		{
+			for (int i = myque.size(); i > 0; --i)//遍历队列中的各个站点
+			{
+				int front_station = myque.front();
+				myque.pop();
+				//下面开始遍历：可以到达front_station的各个路线 或者说 在front_station停站的公交车
+				for (auto& route_index : umap[front_station])//遍历 可以到达 取出队列的第一个元素（某个站点） 的所有路线
+				{
+					if (!have_gone[route_index])//如果该公交车（路线）没有坐过（经过）
+					{
+						have_gone[route_index] = true;//标志位坐过
+						//下面开始遍历：某个公交车（能在front_station停站的公交车）可以到达的所有站点
+						for (auto& station : routes[route_index])//遍历 到达那个站点的 某一条路线
+						{
+							if (station == T)//如果这个公交能到终点，就直接返回mini_steps
+							{
+								return mini_steps;
+							}
+							myque.push(station);//每次都要将该公交车能到达的所有站点插入队列中，来作为后面的一层搜索（广度优先搜索）
+						}
+					}
+				}
+			}
+			++mini_steps;//遍历完一层才++；说明：所有公交车（能在这一层的所有站点停站的公交车）能到达的所有站点中 没有终点
+		}
+		return -1;
+	}
+};
